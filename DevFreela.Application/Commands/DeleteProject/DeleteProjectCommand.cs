353b57819e80d6f9e79f960dc3fe0a22cd0b8748
Caio Cesar Serrano Meneghelli <caiocsmeneghelli@outlook.com>
2022-09-18 18:14:49 -0300

    Ajuste DeleteProjectCommand

--- DevFreela.Application/Commands/DeleteProject/DeleteProjectCommand.cs
+++ DevFreela.Application/Commands/DeleteProject/DeleteProjectCommand.cs
@@ -4,6 +4,11 @@ namespace DevFreela.Application.Commands.DeleteProject
 {
     public class DeleteProjectCommand : IRequest<Unit>
     {
+        public DeleteProjectCommand(int id)
+        {
+            this.Id = id;
+
+        }
         public int Id { get; private set; }
     }
 }
